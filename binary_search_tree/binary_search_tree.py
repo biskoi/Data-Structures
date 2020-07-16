520aa47892e755a12b071a769a73c41fa2b2d77a
biskoi <biskoi@daydream.cafe>
2020-07-15 20:01:17 -0600

    Complete BST day 1

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -17,20 +17,60 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        #val is less than self.val
+        if value < self.value:
+            if self.left is None:
+                self.left = BSTNode(value)
+            #if left child == none, insert val into self.left
+            else:
+            #else repeat op on existing left subtree
+                self.left.insert(value)
+        #val is equal or more than self.val
+        elif value >= self.value:
+            if self.right is None:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
+        
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        #case 1: self.value == target
+        if self.value == target:
+            return True
+        elif target < self.value:
+            if self.left is None:
+                return False
+            else:
+                return self.left.contains(target)
+        elif target >= self.value:
+            if self.right is None:
+                return False
+            else:
+                return self.right.contains(target)
+        else: 
+            return False
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        max_val = self.value
+        current = self
+        while current.right is not None:
+            max_val = current.right.value
+            current = current.right
+        return max_val
+
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        fn(self.value)
+        if self.right is not None:
+            # self.right.for_each(self.right.value)
+            self.right.for_each(fn)
+        if self.left is not None:
+            # self.left.for_each(self.left.value)
+            self.left.for_each(fn)
 
     # Part 2 -----------------------
 
